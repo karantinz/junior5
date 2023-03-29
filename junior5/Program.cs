b06df5fa89152c4ae75084eef318827377ca2658
Zmeegolov <karantinz@mail.ru>
2023-03-29 19:53:37 +0300

    Добавьте файлы проекта.

---
+++ junior5/Program.cs
@@ -0,0 +1,10 @@
+﻿string name = "Anton";
+string lastName = "Red";
+
+Console.WriteLine($"Начальные значения: {name} {lastName}");
+
+string revert = name;
+name = lastName;
+lastName = revert;
+
+Console.WriteLine($"Новые значения: {name} {lastName}");
